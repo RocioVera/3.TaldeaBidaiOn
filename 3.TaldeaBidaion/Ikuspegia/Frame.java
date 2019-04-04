c56ec95ab03d44ecde4de068f954cf0d2f70d08e
Aitor <aitor.nogalesor@elorrieta-errekamari.com>
2019-04-04 14:24:18 +0200

    mensage

--- 3.TaldeaBidaion/Ikuspegia/Frame.java
+++ 3.TaldeaBidaion/Ikuspegia/Frame.java
@@ -9,6 +9,7 @@ public class Frame extends JFrame {
 	GridBagConstraints config = new GridBagConstraints();
 
 	public Frame() {
+		
 		this.setTitle("3.taldearen ostatu zerbitzuen bilatzailea");
 		this.setSize(new Dimension(600, 600));
 		this.setResizable(false); // neurketak ez aldatzeko
